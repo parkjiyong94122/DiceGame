9e1004a8a0c6f9cf0aec7014b3632005440afaf5
parkjiyong94122 <42165663+parkjiyong94122@users.noreply.github.com>
2018-08-17 14:05:08 +0900

    .

---
+++ DiceGame/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(void)
+{
+	// 1. 주사위 게임을 할 맵을 만든다
+
+	// 2.주사위 던지기
+
+	// 3. 주사위 눈에 따라 말 움직이기
+
+	// 4. 2p 생성
+
+	// 5. 승리,패배 판별
+
+	// 6. 지름길,함정 만들기
+
+	return 0;
+}
